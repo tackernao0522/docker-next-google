ac8415e1009075425b5a625d418be018ff8e9387
takaki <takaki55730317@gmail.com>
2024-06-02 13:32:11 +0900

    modify next.confg.mjs

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXTAUTH_URL: "http://localhost:3000",
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "http://localhost:3000",
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   images: {
     domains: ["lh3.googleusercontent.com"],
