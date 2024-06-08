22b9f45c5d915e79515cbb4e8fc6e7601ad8ef38
takaki <takaki55730317@gmail.com>
2024-06-08 17:08:12 +0900

    fixed check-secret/route.ts

---
+++ src/src/app/api/check-secret/rout.ts
@@ -0,0 +1,13 @@
+// src/app/api/check-secret/route.ts
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    const secret = process.env.NEXTAUTH_SECRET;
+    res.status(200).json({ secret });
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
